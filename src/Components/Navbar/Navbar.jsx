f569d65facd3630e0c00cbdddf1811582443e9b8
pitchblendelegit <72884743+pitchblendelegit@users.noreply.github.com>
2024-11-26 21:32:26 +0700

    done

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+import navLogo from '../../assets/nav-logo.svg'
+import navProfile from '../../assets/nav-profile.svg'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img src={navProfile} alt='' className='nav-profile'/>
+    </div>
+  )
+}
+
+export default Navbar
